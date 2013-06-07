b140e917a79ed17c587016737f0321d8b1a9b82b
Michael M. Wasser <michael@raveld.com>
2013-06-07 16:51:33 -0700

    making first test functional -- disabling all other tests for now

--- spec/store/npiRemote.spec.js
+++ spec/store/npiRemote.spec.js
@@ -1,30 +1,38 @@
 var NPIRemote = require('../../lib/store/npiRemote');
 
 describe('NPIRemote', function () {
+  var remote = new NPIRemote();
+
   describe('currentFullID', function () {
     it('should describe the current Full remote dissemination', function (done) {
-      throw 'pending';
+      remote.currentFullID().then(function (value) {
+        expect(value).toNotEqual(null); 
+        done();
+      }).fail(function (error) {
+        expect(error).toEqual(null);
+        done();
+      });
     });
   });
 
   describe('currentWeeklyIDs', function () {
-    it('should describe the current Weekly disseminations', function (done) {
+    xit('should describe the current Weekly disseminations', function (done) {
       throw 'pending';
     });
   });
 
   describe('fetchFull', function () {
-    it('should fetch the current full dissemination', function (done) {
+    xit('should fetch the current full dissemination', function (done) {
       throw 'pending';
     });
   });
 
   describe('fetchWeekly', function () {
-    it('should fetch an available weekly dissemination', function (done) {
+    xit('should fetch an available weekly dissemination', function (done) {
       throw 'pending';
     });
     
-    it('should call error on an invalid weekly dissemination', function (done) {
+    xit('should call error on an invalid weekly dissemination', function (done) {
       throw 'pending';
     });
   });
