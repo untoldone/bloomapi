e924545ca9e62be5ca3160bfa3d95eee4f9bf979
Michael M. Wasser <michael@raveld.com>
2013-06-07 15:23:58 -0700

    adding first file and test, currently not passing

---
+++ TODO.md
@@ -0,0 +1,7 @@
+- list remotely available data
+- transfer files and store locally
+- unzip files
+- list locally available data
+- diff local and remote data
+- pdftohtml to get table metadata
+- html to field metadata
---
+++ spec/store/npiRemote.spec.js
@@ -0,0 +1,31 @@
+var NPIRemote = require('../../lib/store/npiRemote');
+
+describe('NPIRemote', function () {
+  describe('currentFullID', function () {
+    it('should describe the current Full remote dissemination', function (done) {
+      throw 'pending';
+    });
+  });
+
+  describe('currentWeeklyIDs', function () {
+    it('should describe the current Weekly disseminations', function (done) {
+      throw 'pending';
+    });
+  });
+
+  describe('fetchFull', function () {
+    it('should fetch the current full dissemination', function (done) {
+      throw 'pending';
+    });
+  });
+
+  describe('fetchWeekly', function () {
+    it('should fetch an available weekly dissemination', function (done) {
+      throw 'pending';
+    });
+    
+    it('should call error on an invalid weekly dissemination', function (done) {
+      throw 'pending';
+    });
+  });
+});
