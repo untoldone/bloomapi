c9a3857dc98c71c3fd612c09e353e081a9b6c4d1
Michael M. Wasser <michael@raveld.com>
2013-06-14 17:31:06 -0700

    adding code to unzip using p7zip with test

---
+++ spec/store/unzip.spec.js
@@ -0,0 +1,26 @@
+var z = require('../../lib/store/unzip'),
+    child_process = require('child_process'),
+    fakeChildProcessExec = require('../fakeChildProcessExec'),
+    exec;
+
+describe('Unzip', function () {
+  beforeEach(function () {
+    exec = child_process.exec;
+    child_process.exec = fakeChildProcessExec;
+  });
+
+  afterEach(function () {
+    child_process.exec = exec;
+  });
+
+  it('should unzip', function (done) {
+    fakeChildProcessExec.command = "7z e data/hello.zip -odata/test";
+    z.unzip('data/hello.zip', 'data/test')
+      .fail(function (error) {
+        console.log(error.message);
+        expect(error.message).toEqual("Exit code 0");
+      }).done(function () {
+        done();
+      });
+  });
+});
