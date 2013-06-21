e1e0fdcb0e2736c539687bb6ad312c8324a67233
Michael M. Wasser <michael@raveld.com>
2013-06-21 12:47:16 -0700

    switching pdftohtml to pdftotext

--- spec/store/schemaUtilityFixture.js
+++ spec/store/schemaUtilityFixture.js
@@ -2,13 +2,13 @@ var fs = require('fs');
 
 module.exports = {
   full: fs.readFileSync('./spec/store/schemaUtilityFixtureRaw.html', 'utf8'),
-  toSplit: "Hello<br>\nWorld<br>\nTheOther",
+  toSplit: "Hello\nWorld\nTheOther",
   toSplitExpected: ["Hello", "World", "TheOther"],
   toTrim: ["Hello","Data Dissemination Notice", "data1", "data2", "NPPES Data Dissemination - Readme", "Data Dissemination Notice", "data3", "NPPES Data Dissemination - Readme", "World"],
   toTrimExpected: ["data1", "data2", "NPPES Data Dissemination - Readme", "Data Dissemination Notice", "data3"],
   toRemoveBreaks: ['data1', 'NPPES Data Dissemination - Readme', 'junk', 'Data Dissemination Notice', 'data2'],
   toRemoveBreaksExpected: ['data1', 'data2'],
-  toSchema: ['NPI','10','NUMBER', 'Hello World','Some Date', '10 (MM/DD/YYYY)', 'DATE'],
+  toSchema: ['NPI   10   NUMBER', '    Hello World', 'Some Date   10 (MM/DD/YYYY)  DATE'],
   toSchemaExpected: [
     {
       'name': 'NPI',
