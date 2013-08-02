7d6ba363aa1da4e80634707231f9e4606e56101e
Michael M. Wasser <untoldone@users.noreply.github.com>
2013-08-02 11:11:14 -0700

    app understands search for last name, full name, and address

--- www/js/app.js
+++ www/js/app.js
@@ -10,13 +10,33 @@ angular.module('search', []).
 function discoverAPI(term, page) {
   var root = window.location.protocol + "//" + window.location.host + "/api/search?",
       skip = page * 10,
-      part
+      part,
+      match;
+
+  term = term.trim();
+
   if (/^\d{10}$/.exec(term)) {
     // npi
     part = "limit=10&offset=" + skip + "&key1=npi&op1=eq&value1=" + term;
-  } else if (/^\d{5}$/.exec(term)) {
+  } else if (/^(\d{5}|\d{9})$/.exec(term)) {
     // zipcode
     part = "limit=10&offset=" + skip + "&key1=practice_address.zip&op1=eq&value1=" + term;
+  } else if (/^([a-zA-Z]+)$/.exec(term)) {
+    // lastname
+    part = "limit=10&offset=" + skip + "&key1=last_name&op1=eq&value1=" + term.toUpperCase();
+  } else if (match = /^([a-zA-Z]+)((\s+[a-zA-Z]+)+)$/.exec(term)) {
+    part = "limit=10&offset=" + skip + "&key1=last_name&op1=eq&value1=" + match[2].trim().toUpperCase() +
+           "&key2=first_name&op2=eq&value2=" + match[1].trim().toUpperCase();
+  } else if (match = /^([\d\w\s]+),\s*(\w+),\s*(\w+)\s*(\d+)$/.exec(term)) {
+    var address_line = match[1].trim().toUpperCase(),
+        city = match[2].trim().toUpperCase(),
+        state = match[3].trim().toUpperCase(),
+        zip = match[4].trim().toUpperCase();
+    part = "limit=10&offset=" + skip + 
+           "&key1=practice_address.address_line&op1=eq&value1=" + address_line +
+           "&key2=practice_address.city&op2=eq&value2=" + city +
+           "&key3=practice_address.state&op3=eq&value3=" + state +
+           "&key4=practice_address.zip&op4=eq&value4=" + zip;
   }
 
   return root + part;
