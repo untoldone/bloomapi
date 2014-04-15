4e0f5e130677bc8eb624c8fc1b85eeed07ecdf91
Michael M. Wasser <michael.m.wasser@gmail.com>
2014-04-15 16:24:02 -0700

    adding support for custom actions

---
+++ sources/npi/actions/fix_npi/index.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+
+};
+
+module.exports.prototype = {
+
+};
--- spec/actions/parser.spec.js
+++ spec/actions/parser.spec.js
@@ -1,5 +1,6 @@
 var Parser = require('../../lib/actions/parser'),
-    Download = require('../../actions/download');
+    Download = require('../../actions/download'),
+    FixNpi = require('../../sources/npi/actions/fix_npi');
 
 describe('actions/parser', function () {
   it('should return an action', function () {
@@ -11,6 +12,20 @@ describe('actions/parser', function () {
     });
 
     expect(results.action === Download).toEqual(true);
+    expect(results.parameters).toEqual({
+      url: "http://www.hello.com/q=world"
+    });
+  });
+
+  it('should return a custom action when used', function () {
+    var parser = new Parser({ one: "world", dir: __dirname + "/../../sources/npi" }),
+        results;
+
+    results = parser.parse({
+      fix_npi: "url=http://www.hello.com/q={{one}}"
+    });
+
+    expect(results.action === FixNpi).toEqual(true);
     expect(results.parameters).toEqual({
       url: "http://www.hello.com/q=world"
     });
