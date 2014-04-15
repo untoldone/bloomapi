c378233d5105cd19ead05d86ef214a658a336a54
Michael M. Wasser <michael.m.wasser@gmail.com>
2014-04-15 15:01:26 -0700

    adding parameter parser for source ymls

---
+++ spec/actions/parameters.spec.js
@@ -0,0 +1,39 @@
+var Parameters = require('../../lib/actions/parameters');
+
+describe('actions/parameters', function () {
+  it('should parse one string parameter', function () {
+    var parameters = new Parameters(),
+        results;
+    
+    results = parameters.parse("   hello=world   ");
+
+    expect(results).toEqual({
+      "hello": "world"
+    });
+  });
+
+  it('should parse two string parameters', function () {
+    var parameters = new Parameters(),
+        results;
+    
+    results = parameters.parse("  hello=world    one=http://www.hello.com/q=hello%20world   ");
+
+    expect(results).toEqual({
+      "hello": "world",
+      "one": "http://www.hello.com/q=hello%20world"
+    });
+  });
+
+  it('should parse object parameters', function () {
+    var parameters = new Parameters(),
+        results;
+    
+    results = parameters.parse({
+      "hello": "world" 
+    });
+
+    expect(results).toEqual({
+      "hello": "world"
+    });
+  });
+});
